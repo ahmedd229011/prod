d589ffae6d5f3466a5bdeafe24268eb8959b3919
Ahmed Nageh <Ahmed.Nageh@intercom.com.eg>
2025-07-02 14:47:01 +0300

    initial

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,15 @@
+import { NgFor } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
   templateUrl: './app.html',
+  imports:[NgFor],
   styleUrl: './app.scss'
 })
 export class App {
   protected title = 'prod-app';
+  salesData = [65, 59, 80, 81, 56, 55]; // Percent heights
+
 }
